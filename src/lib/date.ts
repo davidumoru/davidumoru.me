1af86e0a88a2e7551194edf478e27db222b02419
David Umoru <umorudavido@gmail.com>
2025-01-06 17:32:45 +0100

    Remove timeStyle from date formatting

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,6 @@
 export function displayDate(date: Date) {
 	return new Intl.DateTimeFormat('en-GB', {
 		dateStyle: 'medium',
-        timeStyle: 'short',
 		timeZone: 'Africa/Lagos',
 	}).format(date)
 }
