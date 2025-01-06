c0d39ae62efed17f8aeca735f4b2f4c842e639cb
David Umoru <umorudavido@gmail.com>
2025-01-06 15:05:30 +0100

    Add note, project components and pages

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  projects: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      year: z.number(),
+      type: z.string(),
+      description: z.string(),
+      highlight: z.boolean().optional(),
+    }),
+  }),
+  posts: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+    }),
+  }),
+};
