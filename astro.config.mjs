8951cce956071fb611b8f4a7575532a8b8e1b409
David Umoru <umorudavido@gmail.com>
2025-01-07 02:29:35 +0100

    Add MDX support and externalize plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import externalize from "./src/plugins/externalize";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
+  integrations: [mdx()],
+  markdown: {
+    syntaxHighlight: "prism",
+    rehypePlugins: [[externalize, { domain: "davidumoru.me" }]],
+  },
+  redirects: {
+    "/live": { status: 302, destination: "https://twitch.tv/thedavidumoru" },
+  },
 });
---
+++ src/plugins/externalize.ts
@@ -0,0 +1,23 @@
+import type { RehypePlugin } from "@astrojs/markdown-remark";
+import { visit } from "unist-util-visit";
+
+const externalize: RehypePlugin = (options: { domain: string }) => {
+  return (tree) => {
+    visit(tree, (node) => {
+      if (node.type != "element") {
+        return;
+      }
+
+      const href = node?.properties?.href as string | undefined;
+      if (!(node.tagName == "a" && !!href)) {
+        return;
+      }
+
+      if (!href.includes(options.domain)) {
+        node.properties["target"] = "_blank";
+      }
+    });
+  };
+};
+
+export default externalize;
