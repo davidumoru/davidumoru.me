3bcbc73c782a4e1b00645815ccb4d3c024123ace
David Umoru <umorudavido@gmail.com>
2025-01-07 03:18:11 +0100

    Add vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import { defineConfig } from "astro/config";
 import externalize from "./src/plugins/externalize";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
   integrations: [mdx()],
+
   markdown: {
     syntaxHighlight: "prism",
     rehypePlugins: [[externalize, { domain: "davidumoru.me" }]],
   },
-  redirects: {
-    "/live": { status: 302, destination: "https://twitch.tv/thedavidumoru" },
-  },
+
+  adapter: vercel(),
 });
