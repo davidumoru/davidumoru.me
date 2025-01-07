555039824ca80d7b674ffb18d0ec60392cbcc512
David Umoru <umorudavido@gmail.com>
2025-01-07 09:49:25 +0100

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@
 import { defineConfig } from "astro/config";
 import externalize from "./src/plugins/externalize";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
-  integrations: [mdx()],
-
+  site: "https://davidumoru.me",
+  integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: "prism",
     rehypePlugins: [[externalize, { domain: "davidumoru.me" }]],
