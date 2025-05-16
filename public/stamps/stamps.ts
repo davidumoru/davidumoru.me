22b85a19841fe8902df30705ddb035abdd345e6d
David Umoru <umorudavido@gmail.com>
2025-05-16 01:24:11 +0100

    Update image URLs in stampData to use Cloudinary for improved performance

--- public/stamps/stamps.ts
+++ public/stamps/stamps.ts
@@ -2,293 +2,293 @@ export const stampData = [
   {
     country: "United States",
     imageUrl:
-      "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/United_States_xom8ae.jpg",
     hue: 210,
   },
   {
     country: "India",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/taj-majal-at-sunrise-in-agra-india-royalty-free-image-1719601420.jpg?crop=0.66667xw:1xh;center,top&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352590/India_xkavu4.jpg",
     hue: 30,
   },
   {
     country: "China",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/view-of-winding-road-of-tianmen-mountain-national-royalty-free-image-1719603638.jpg?crop=0.668xw:1.00xh;0.243xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352588/China_vp3tox.jpg",
     hue: 0,
   },
   {
     country: "United Kingdom",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/big-ben-with-bridge-over-thames-and-flag-of-england-royalty-free-image-1719603543.jpg?crop=0.668xw:1.00xh;0,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/United_Kingdom_aog0xm.jpg",
     hue: 230,
   },
   {
     country: "Canada",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Moraine_Lake_17092005.jpg/1280px-Moraine_Lake_17092005.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352587/Canada_wkha0g.jpg",
     hue: 355,
   },
   {
     country: "Germany",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Brandenburger_Tor_abends.jpg/1280px-Brandenburger_Tor_abends.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352590/Germany_qsv4xr.jpg",
     hue: 50,
   },
   {
     country: "Brazil",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/mata-atlantica-atlantic-forest-in-brazil-royalty-free-image-1719601616.jpg?crop=0.668xw:1.00xh;0.172xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Brazil_hy5ct1.jpg",
     hue: 120,
   },
   {
     country: "France",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/spectacular-colorful-traditional-french-houses-on-royalty-free-image-1719599675.jpg?crop=0.668xw:1.00xh;0.332xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352590/France_mmo2w6.jpg",
     hue: 220,
   },
   {
     country: "Russia",
     imageUrl:
-      "https://www.usnews.com/object/image/00000151-8442-d104-a7f5-aeee9c7e0000/bc16-russia-crop-editorial.jpg?update-time=1452285795193&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352597/Russia_ga8wpl.jpg",
     hue: 5,
   },
   {
     country: "Australia",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/scenic-view-of-rock-formations-kalbarri-national-royalty-free-image-1719600482.jpg?crop=0.66635xw:1xh;center,top&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352584/Australia_hjhape.jpg",
     hue: 215,
   },
   {
     country: "Mexico",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/narrow-street-in-the-old-town-of-san-miguel-de-royalty-free-image-1719599371.jpg?crop=0.66635xw:1xh;center,top&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352595/Mexico_eib1am.jpg",
     hue: 130,
   },
   {
     country: "Japan",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/f8c0b3c6-e540-47c6-b410-d773300f46c2.jpeg?crop=1xw:1xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352592/Japan_eqqjo0.jpg",
     hue: 350,
   },
   {
     country: "Philippines",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/landscape-of-coron-busuanga-island-palawan-province-royalty-free-image-1719602776.jpg?crop=0.668xw:1.00xh;0.294xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352596/Philippines_x3bvfi.jpg",
     hue: 240,
   },
   {
     country: "Indonesia",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/pura-ulun-danu-bratan-temple-in-bali-royalty-free-image-1719601267.jpg?crop=0.668xw:1.00xh;0.109xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352592/Indonesia_hgurr5.jpg",
     hue: 358,
   },
   {
     country: "Italy",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1280px-Colosseo_2020.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352593/Italy_qjn12f.jpg",
     hue: 110,
   },
   {
     country: "Nigeria",
     imageUrl:
-      "https://cdn.britannica.com/14/218214-131-318B2ACB/National-Theatre-Lagos-Nigeria.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352595/Nigeria_omtebx.jpg",
     hue: 125,
   },
   {
     country: "Pakistan",
     imageUrl:
-      "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,q_auto,w_720/641edf446abaab001eafb4c0.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352595/Pakistan_li48cw.jpg",
     hue: 140,
   },
   {
     country: "Turkey",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Hagia_Sophia_Mars_2013.jpg/1280px-Hagia_Sophia_Mars_2013.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Turkey_gfr00v.jpg",
     hue: 359,
   },
   {
     country: "South Korea",
     imageUrl:
-      "https://www.usnews.com/object/image/00000151-844d-d104-a7f5-aeedb78a0000/bc16-south-korea-crop-editorial.jpg?update-time=1452285597833&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352584/South_Korea_nr47pf.jpg",
     hue: 250,
   },
   {
     country: "Spain",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/6aee784e-6116-492b-b591-e8df574abd2c.jpeg?crop=1xw:1xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352585/Spain_zt2maj.jpg",
     hue: 45,
   },
   {
     country: "Netherlands",
     imageUrl:
-      "https://traversewithtaylor.com/wp-content/uploads/2023/08/2-copy-2.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352594/Netherlands_ztp1ir.jpg",
     hue: 25,
   },
   {
     country: "South Africa",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/twelve-apostles-mountain-in-camps-bay-cape-town-royalty-free-image-1719600777.jpg?crop=0.668xw:1.00xh;0,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/South_Africa_xoj5ra.jpg",
     hue: 160,
   },
   {
     country: "Vietnam",
     imageUrl:
-      "https://www.outlooktravelmag.com/media/vietnam-1-1611926800.profileImage.2x-jpg-webp.webp",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352586/Vietnam_xjln0s.webp",
     hue: 10,
   },
   {
     country: "Egypt",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/All_Gizah_Pyramids.jpg/1280px-All_Gizah_Pyramids.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352590/Egypt_efg2gi.jpg",
     hue: 40,
   },
   {
     country: "Bangladesh",
     imageUrl:
-      "https://www.usnews.com/object/image/00000183-5b67-d6bf-afdb-5f6fa5830000/220927bc.bangladesh.profile.jpg?update-time=1663685958902&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352584/Bangladesh_nq4n1y.jpg",
     hue: 135,
   },
   {
     country: "Ukraine",
     imageUrl:
-      "https://www.usnews.com/object/image/00000151-8464-d104-a7f5-aeecae7c0000/bc16-ukraine-crop-editorial.jpg?update-time=1449625233781&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352586/Ukraine_jftc9x.jpg",
     hue: 60,
   },
   {
     country: "Saudi Arabia",
     imageUrl:
-      "https://www.usnews.com/object/image/00000151-8444-d104-a7f5-aeecfabb0000/bc16-saudi-arabia-crop-editorial.jpg?update-time=1452285908643&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Saudi_Arabia_ce9xox.jpg",
     hue: 115,
   },
   {
     country: "Argentina",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/the-perito-moreno-glacier-royalty-free-image-1719601976.jpg?crop=0.668xw:1.00xh;0.332xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Argentina_d8hik9.jpg",
     hue: 195,
   },
   {
     country: "Poland",
     imageUrl:
-      "https://www.usnews.com/object/image/0000015a-397b-d9e9-a9da-bf7fed970000/bc17-poland-crop.jpg?update-time=1487098372148&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352597/Poland_lcamf3.jpg",
     hue: 357,
   },
   {
     country: "Malaysia",
     imageUrl:
-      "https://www.usnews.com/object/image/00000151-8405-d104-a7f5-aeed037a0000/151208-editorial.jpg?update-time=1449618961007&size=responsiveGallery",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352593/Malaysia_wy2uty.jpg",
     hue: 55,
   },
   {
     country: "Norway",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/perfect-reflection-of-the-reine-village-on-the-royalty-free-image-1719599055.jpg?crop=0.668xw:1.00xh;0.262xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352595/Norway_nzzdr5.jpg",
     hue: 205,
   },
   {
     country: "Turks and Caicos",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/marina-at-sunrise-with-luxury-yachts-in-the-turks-royalty-free-image-1719599190.jpg?crop=0.750xw:1.00xh;0.250xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352585/Turks_and_Caicos_jwf6cm.jpg",
     hue: 175,
   },
   {
     country: "Seychelles",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/anse-sous-dargent-beach-with-granite-boulders-royalty-free-image-1719600613.jpg?crop=0.670xw:1.00xh;0.121xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352583/Seychelles_uqsrbs.jpg",
     hue: 190,
   },
   {
     country: "Croatia",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/adriatic-sea-dubrovnik-landscape-royalty-free-image-1719602044.jpg?crop=0.668xw:1.00xh;0.236xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352588/Croatia_y8vjfs.jpg",
     hue: 15,
   },
   {
     country: "Portugal",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/porto-portugal-skyline-royalty-free-image-1719602672.jpg?crop=0.668xw:1.00xh;0.332xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352598/Portugal_qhgknz.jpg",
     hue: 350,
   },
   {
     country: "Sweden",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/bathing-in-sea-sk-rhamn-tjorn-sweden-royalty-free-image-1719603065.jpg?crop=0.665xw:0.997xh;0,0.00255xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352585/Sweden_egxgfq.jpg",
     hue: 200,
   },
   {
     country: "Morocco",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/ait-benhaddou-ancient-city-in-morocco-north-africa-royalty-free-image-1719603392.jpg?crop=0.668xw:1.00xh;0.158xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352595/Morocco_mgzgxl.jpg",
     hue: 20,
   },
   {
     country: "Colombia",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/colourful-architecture-in-guatape-royalty-free-image-1719603708.jpg?crop=0.668xw:1.00xh;0.206xw,0&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352588/Colombia_sujkij.jpg",
     hue: 70,
   },
   {
     country: "Chile",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/37f27b65-ae41-4c60-af50-2f01c3e3eddb.jpeg?crop=1xw:0.999xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352587/Chile_uvjiew.jpg",
     hue: 205,
   },
   {
     country: "Ireland",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/85d25266-5ef5-4079-8e77-5138f161e1ff.jpeg?crop=1xw:0.987xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352592/Ireland_imippd.jpg",
     hue: 140,
   },
   {
     country: "Mauritius",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/ab44b54f-b666-4c26-9dc7-5da7fbebb209.jpeg?crop=1xw:1xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352593/Mauritius_c5qju2.jpg",
     hue: 170,
   },
   {
     country: "Switzerland",
     imageUrl:
-      "https://hips.hearstapps.com/hmg-prod/images/1133bb5a-d0eb-40f9-a3db-ba83a0e1e5bf.jpeg?crop=1xw:1xh;0xw,0xh&resize=980:*",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Switzerland_sdm0wo.jpg",
     hue: 215,
   },
   {
     country: "Cameroon",
     imageUrl:
-      "https://images.pexels.com/photos/17290979/pexels-photo-17290979/free-photo-of-roundabout-in-yaounde.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352584/Cameroon_z1wk8c.jpg",
     hue: 60,
   },
   {
     country: "Kenya",
     imageUrl:
-      "https://media-cdn.tripadvisor.com/media/photo-s/17/72/7a/16/the-beautiful-city-of.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352592/Kenya_iuoypd.jpg",
     hue: 40,
   },
   {
     country: "Ghana",
     imageUrl:
-      "https://images.goway.com/production/styles/wide/s3/hero_image/AdobeStock_341274506.jpeg?VersionId=q9f54FWaZfIN2994NjK5e36WyP7kPMXr&itok=vCy37sMw",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352591/Ghana_zmrgmi.jpg",
     hue: 210,
   },
   {
     country: "Ethiopia",
     imageUrl:
-      "https://cloudfront.safaribookings.com/blog/2023/08/04-20_of_the_Best_and_Most_Beautiful_Cities_in_Africa-BW-1600px.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352590/Ethiopia_hoken5.jpg",
     hue: 25,
   },
   {
     country: "Senegal",
     imageUrl:
-      "https://thumbs.dreamstime.com/b/overview-dakar-aerial-view-to-shore-atlantic-ocean-senegal-africa-62895866.jpg",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352582/Senegal_qkv1pm.jpg",
     hue: 220,
   },
   {
     country: "Algeria",
     imageUrl:
-      "https://media.istockphoto.com/id/96064783/photo/constantine-adventure.jpg?s=612x612&w=0&k=20&c=ar7vbGkK7YBKy8qhDQYWyMgRHt4MIFXFj05hVlsbx2U=",
+      "http://res.cloudinary.com/deha0jqdf/image/upload/v1747352581/Algeria_esqqmn.jpg",
     hue: 35,
   },
   {
     country: "Other",
-    hue: 360
+    hue: 360,
   },
 ];
