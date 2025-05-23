a66e298d62f98f23070efcf49978fb065d0f72f7
David Umoru <umorudavido@gmail.com>
2025-05-23 18:48:19 +0100

    Add Taiwan and Peru to stamps.ts

--- public/stamps/stamps.ts
+++ public/stamps/stamps.ts
@@ -294,6 +294,18 @@ export const stampData = [
     hue: 21,
   },
   {
+    country: "Taiwan",
+    imageUrl:
+      "https://res.cloudinary.com/deha0jqdf/image/upload/v1748022435/Taiwan_k9ww99.webp",
+    hue: 21,
+  },
+  {
+    country: "Peru",
+    imageUrl:
+      "https://res.cloudinary.com/deha0jqdf/image/upload/v1748022436/Peru_wlckkl.webp",
+    hue: 21,
+  },
+  {
     country: "Other",
     hue: 360,
   },
