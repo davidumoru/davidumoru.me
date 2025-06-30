21f0e421377ada5b25e57077f3b71f3c629024d3
David Umoru <umorudavido@gmail.com>
2025-06-30 23:15:22 +0100

    Rename 'writing' to 'article' in content config and update related references in posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,7 +31,7 @@ export const collections = {
         description: z.string(),
         datePublished: z.date(),
         dateModified: z.date().optional(),
-        writing: z.boolean(),
+        article: z.boolean(),
         img: image().array().optional(),
         imgAlt: z.string().optional(),
         ogImage: image().optional(),
