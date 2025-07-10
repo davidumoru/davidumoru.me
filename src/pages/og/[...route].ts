454847615723d3ccab86555be34c725e2d6ccccd
David Umoru <umorudavido@gmail.com>
2025-07-10 22:22:19 +0100

    Refactor OG image generation to separate collection entries and static pages

--- src/pages/og/[...route].ts
+++ src/pages/og/[...route].ts
@@ -1,34 +1,62 @@
 import { OGImageRoute } from "astro-og-canvas";
 import { getCollection } from "astro:content";
 
-const collectionEntries = await getCollection("posts");
+const postEntries = await getCollection("posts");
+const labEntries = await getCollection("lab");
+const pageEntries = await getCollection("pages");
 
-const pages = Object.fromEntries(
-  collectionEntries.map(({ id, data }) => [id, data])
-);
+const staticPages = {
+  index: {
+    title: "David Umoru",
+  },
+  bookshelf: {
+    title: "Bookshelf",
+  },
+  guestbook: {
+    title: "Guestbook",
+  },
+  now: {
+    title: "Now",
+  },
+  projects: {
+    title: "Projects",
+  },
+  webrings: {
+    title: "Webrings",
+  },
+  lab: {
+    title: "Lab",
+  },
+  posts: {
+    title: "Posts",
+  },
+};
+
+const collectionPages = [...postEntries, ...labEntries, ...pageEntries];
+
+const pages = {
+  ...Object.fromEntries(collectionPages.map(({ id, data }) => [id, data])),
+  ...staticPages,
+};
 
 export const { getStaticPaths, GET } = OGImageRoute({
   param: "route",
-
-  pages: pages,
-
-  getImageOptions: (route, page) => {
-    return {
-      title: page.title,
-      description: "davidumoru.me",
-      bgGradient: [[255, 255, 255]],
-      font: {
-        title: {
-          color: [0, 0, 0],
-          size: 72,
-          weight: "Bold",
-        },
-        description: {
-          color: [100, 100, 100],
-          size: 30,
-        },
+  pages,
+  getImageOptions: (route, page) => ({
+    title: page.title,
+    description: "davidumoru.me",
+    bgGradient: [[255, 255, 255]],
+    font: {
+      title: {
+        color: [0, 0, 0],
+        size: 72,
+        weight: "Bold",
       },
-      padding: 100,
-    };
-  },
+      description: {
+        color: [100, 100, 100],
+        size: 30,
+      },
+    },
+    padding: 100,
+  }),
 });
