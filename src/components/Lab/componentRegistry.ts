a972e2ffa96c36409de0b707dab824101d065913
David Umoru <umorudavido@gmail.com>
2025-10-29 21:38:10 +0100

    Change all naming case for lab components

--- src/components/Lab/componentRegistry.ts
+++ src/components/Lab/componentRegistry.ts
@@ -1,9 +1,9 @@
-import AnimatedCardsCarousel from "./Experiments/AnimatedCardsCarousel";
-import HoveringProfiles from "./Experiments/HoveringProfiles";
-import StackedAvatars from "./Experiments/StackedAvatars";
-import PillButton from "./Experiments/PillButton";
-import SwipeableCardStack from "./Experiments/SwipeableCardStack";
-import AnimatedTabs from "./Experiments/AnimatedTabs"
+import AnimatedCardsCarousel from "./Experiments/animated-cards-carousel";
+import HoveringProfiles from "./Experiments/hovering-profiles";
+import StackedAvatars from "./Experiments/stacked-avatars";
+import PillButton from "./Experiments/pill-button";
+import SwipeableCardStack from "./Experiments/swipeable-card-stack";
+import AnimatedTabs from "./Experiments/animated-tabs"
 
 export const componentRegistry = {
   AnimatedCardsCarousel,
