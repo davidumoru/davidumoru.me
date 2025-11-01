4fd184de112a6e244e7e262740bc7b201f7ac911
David Umoru <umorudavido@gmail.com>
2025-11-01 15:50:09 +0100

    Make link column optional in GuestBook table and update insert logic to handle optional link

--- db/config.ts
+++ db/config.ts
@@ -4,7 +4,7 @@ const GuestBook = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     author: column.text(),
-    link: column.text(),
+    link: column.text({ optional: true }),
     content: column.text(),
     country: column.text(),
     timestamp: column.date({ default: NOW }),
