b3640c38eba442f1016d7dc946ea1de8d994af29
David Umoru <umorudavido@gmail.com>
2026-06-17 23:33:19 +0100

    update stro configs, add inter font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://davidumoru.me",
+  prefetch: true,
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: ["100 900"],
+    },
+  ],
+});
