580b66c2aae228ac80f996f12e451a43691b6dd4
David Umoru <umorudavido@gmail.com>
2026-06-26 23:42:14 +0100

    add markdown processing support with unified

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import rehypeExternalLinks from "rehype-external-links";
+import { unified } from "@astrojs/markdown-remark";
 
 // https://astro.build/config
 export default defineConfig({
@@ -36,17 +37,19 @@ export default defineConfig({
   ],
 
   markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-          rel: ["noopener"],
-          content: { type: "text", value: " (opens in new tab)" },
-          contentProperties: { className: ["sr-only"] },
-        },
+    processor: unified({
+      rehypePlugins: [
+        [
+          rehypeExternalLinks,
+          {
+            target: "_blank",
+            rel: ["noopener"],
+            content: { type: "text", value: " (opens in new tab)" },
+            contentProperties: { className: ["sr-only"] },
+          },
+        ],
       ],
-    ],
+    }),
     shikiConfig: {
       themes: {
         light: "github-light",
