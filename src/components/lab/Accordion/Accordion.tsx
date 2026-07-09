db749946533fc7d7534dcb5f198b6caab4461d1a
David Umoru <umorudavido@gmail.com>
2026-07-09 21:33:40 +0100

    update SVG attributes in accordion experiment

--- src/components/lab/Accordion/Accordion.tsx
+++ src/components/lab/Accordion/Accordion.tsx
@@ -46,9 +46,9 @@ export default function Accordion() {
                   height="16"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M9 6l6 6-6 6" />
                 </svg>
